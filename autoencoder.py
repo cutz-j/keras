ff3ff6881e46a483aa1498e8efb41f25488661a8
Hyeonseong Jeon <cutz309@gmail.com>
2019-04-10 14:03:03 +0900

    Create autoencoder.py

---
+++ autoencoder.py
@@ -0,0 +1,26 @@
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.datasets import mnist
+import numpy as np
+
+(x_train, _), (x_test, _) = mnist.load_data()
+
+encoding_dim = 32
+
+input_img = Input(shape=(784,))
+encoded = Dense(encoding_dim, activation='relu')(input_img)
+decoded = Dense(784, activation='sigmoid')(encoded)
+
+autoencoder = Model(input_img, decoded)
+encoder = Model(input_img, encoded)
+encoded_input = Input(shape=(encoding_dim,))
+decoder_layer = autoencoder.layers[-1]
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+x_train = x_train.astype(np.float32) / 255.
+x_test = x_test.astype(np.float32) / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+autoencoder.fit(x_train, x_train, epochs=50, batch_size=256, shuffle=True, validation_data=(x_test, x_test))
